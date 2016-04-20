374d8030af1a9fb102ce6b789552012edc172c2d
mlollis99 <mattlollis99@gmail.com>
2016-04-20 00:34:04 -0400

    Added files via upload

--- TwitterPlotter.R
+++ TwitterPlotter.R
@@ -5,18 +5,13 @@ library(RCurl)
 library(RJSONIO)
 library(stringr)
 library(quanteda)
-# 
-# sanders <- matrix(c(1,2,0.2344158,0.2931692, 0.2374362, 0.2766842, 0.256400889, 0.2048006338, .49, .45), nrow=2, ncol=5)
-# trump <- matrix(c(1,2,0.6178285,0.6874033, 0.1000708, 0.1991484, 0.5510507255, 0.6390417982, .4, .43), nrow=2, ncol=5)
-# cruz <- matrix(c(1,2,0.3377648,0.2950204, 0.2626514, 0.2814825, 0.06581490189, 0.05523616319, .35, .22), nrow=2, ncol=5)
-# clinton <- matrix(c(1,2,0.2748298,0.2661213, 0.3154153, 0.2756584, 0.1268212702, 0.1009214048, .51, .55), nrow=2, ncol=5)
 
-candidates <- matrix(c("Sanders", "Trump", "Cruz", "Clinton", 0.2637925, 0.6526159, 0.3163926, 0.27047555, 0.2570602, 0.1496096, 0.27206695, 0.29553685, 0.2306007614, 0.5950462619, 0.06052553254, 0.1138713375, 0.47, .415, 0.285, 0.53), nrow=4, ncol=5)
+candidates <- matrix(c("Sanders", "Trump", "Cruz", "Clinton", 0.2637925, 0.6526159, 0.3163926, 0.27047555, 0.2570602, 0.1496096, 0.27206695, 0.29553685, 0.2306007614, 0.5950462619, 0.06052553254, 0.1138713375, 0.47, .415, 0.285, 0.53, .42, .6, .15, .58), nrow=4, ncol=6)
 candidates
 
 # plot(lm(sanders[,1] ~ sanders[,2] + sanders[,3] + sanders[,4]))
-plot(candidates[,2], candidates[,5], xlab="Positive Sentiment Ratio", ylab="Poll Results Ratio", main="Positive Sentiment vs. Poll Results", col=c("black","blue","red","green"))
-model <- lm(candidates[,5] ~ as.numeric(candidates[,4]))
+plot(candidates[,2], candidates[,5], xlab="Positive Sentiment Tweet Ratio", ylab="Opinion Poll Results Ratio", main="Positive Sentiment Tweet Ratio vs. Opinion Poll Results", col=c("black","blue","red","green"))
+model <- lm(candidates[,5] ~ as.numeric(candidates[,2]))
 summary(model)
 abline(model, col="red")
 #Sanders = black, Trump = blue, Cruz = Red, Clinton = Green
@@ -26,8 +21,19 @@ model <- lm(candidates[,5] ~ as.numeric(candidates[,3]))
 summary(model)
 abline(model, col="red")
 
-plot(candidates[,4], candidates[,5], xlab="Total Tweet Ratio", ylab="Poll Results Ratio", main="Total Tweets vs. Poll Results", col=c("black","blue","red","green"))
+plot(candidates[,4], candidates[,5], xlab="Total Tweet Ratio", ylab="Opinion Poll Results Ratio", main="Total Tweet Ratio vs. Opinion Poll Results", col=c("black","blue","red","green"))
 model <- lm(candidates[,5] ~ as.numeric(candidates[,4]))
 summary(model)
 abline(model, col="red")
 
+#NY Primary Results
+plot(candidates[,2], candidates[,6], xlab="Positive Sentiment Tweet Ratio", ylab="NY Primary Results Ratio", main="Positive Sentiment Tweet Ratio vs. NY Primary Results", col=c("black","blue","red","green"))
+model <- lm(candidates[,6] ~ as.numeric(candidates[,2]))
+summary(model)
+abline(model, col="red")
+
+
+plot(candidates[,4], candidates[,6], xlab="Total Tweet Ratio", ylab="NY Primary Results Ratio", main="Total Tweet Ratio vs. NY Primary Results", col=c("black","blue","red","green"))
+model <- lm(candidates[,6] ~ as.numeric(candidates[,4]))
+summary(model)
+abline(model, col="red")
